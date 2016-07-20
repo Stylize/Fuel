9d097930ffe5b3074e95a958a3f3974560c93dc9
Albert De Guzman <adeguzman@launchbrigade.com>
2016-07-19 22:30:43 -0700

    fix bug if timber plugin is not found

--- functions.php
+++ functions.php
@@ -7,6 +7,15 @@
  *
  */
 
+//Run this code if timber is not found.
+if (! class_exists('Timber')) {
+	add_action('admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+	});
+
+	return;
+}
+
 spl_autoload_register('autoloader');
 function autoloader($class_name)
 {
